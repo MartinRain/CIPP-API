7f565ff7aa8a42a6dc83cd77446860f554194415
MartinRain <35352705+MartinRain@users.noreply.github.com>
2026-05-06 18:40:35 -0400

    Update Invoke-CIPPStandardEntraDeviceJoinScope.ps1
    PUT instead of PUSH

--- Modules/CIPPStandards/Public/Standards/Invoke-CIPPStandardEntraDeviceJoinScope.ps1
+++ Modules/CIPPStandards/Public/Standards/Invoke-CIPPStandardEntraDeviceJoinScope.ps1
@@ -14,7 +14,7 @@ function Invoke-CIPPStandardEntraDeviceJoinScope {
 
     try {
         $CurrentInfo = New-GraphGetRequest `
-            -uri 'https://graph.microsoft.com/beta/policies/deviceRegistrationPolicy/deviceRegistrationPolicy' `
+            -uri 'https://graph.microsoft.com/beta/policies/deviceRegistrationPolicy' `
             -tenantid $Tenant
     } catch {
         $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
@@ -31,13 +31,25 @@ function Invoke-CIPPStandardEntraDeviceJoinScope {
             return
         }
 
-        $Group = New-GraphGetRequest `
-            -Uri "https://graph.microsoft.com/beta/groups?`$top=999&`$select=id,displayName&`$filter=displayName eq '$($Settings.customGroup)'" `
-            -tenantid $Tenant `
-            -asApp $true
+        try {
+            $GroupResult = New-GraphGetRequest `
+                -Uri "https://graph.microsoft.com/beta/groups?`$top=999&`$select=id,displayName&`$filter=displayName eq '$($Settings.customGroup)'" `
+                -tenantid $Tenant `
+                -asApp $true
 
-        $ExpectedGroupId = ($Group.value ?? $Group | Select-Object -First 1).id
-        $ExpectedGroupName = ($Group.value ?? $Group | Select-Object -First 1).displayName
+            $Group = if ($GroupResult.value) {
+                $GroupResult.value | Select-Object -First 1
+            } else {
+                $GroupResult | Select-Object -First 1
+            }
+
+            $ExpectedGroupId = $Group.id
+            $ExpectedGroupName = $Group.displayName
+        } catch {
+            $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+            Write-LogMessage -API 'Standards' -Tenant $Tenant -Message "Could not find group '$($Settings.customGroup)'. Error: $ErrorMessage" -Sev Error
+            return
+        }
 
         if (-not $ExpectedGroupId) {
             Write-LogMessage -API 'Standards' -Tenant $Tenant -Message "Group '$($Settings.customGroup)' was not found." -Sev Error
@@ -47,6 +59,13 @@ function Invoke-CIPPStandardEntraDeviceJoinScope {
 
     $CurrentAllowed = $CurrentInfo.azureADJoin.allowedToJoin
 
+    $CurrentGroupIds = @()
+    if ($CurrentAllowed.groups) {
+        $CurrentGroupIds = @($CurrentAllowed.groups)
+    } elseif ($CurrentAllowed.groupIds) {
+        $CurrentGroupIds = @($CurrentAllowed.groupIds)
+    }
+
     $StateIsCorrect = switch ($Settings.appliesTo) {
         'all' {
             $CurrentAllowed.appliesTo -eq 'all'
@@ -55,8 +74,7 @@ function Invoke-CIPPStandardEntraDeviceJoinScope {
             $CurrentAllowed.appliesTo -eq 'none'
         }
         'selected' {
-            ($CurrentAllowed.appliesTo -eq 'selected') -and
-            ($CurrentAllowed.groups -contains $ExpectedGroupId -or $CurrentAllowed.groupIds -contains $ExpectedGroupId)
+            ($CurrentAllowed.appliesTo -eq 'selected') -and ($CurrentGroupIds -contains $ExpectedGroupId)
         }
         default {
             $false
@@ -66,6 +84,7 @@ function Invoke-CIPPStandardEntraDeviceJoinScope {
     $CompareField = [PSCustomObject]@{
         appliesTo   = $CurrentAllowed.appliesTo
         customGroup = $ExpectedGroupName
+        groupId     = $ExpectedGroupId
     }
 
     if ($Settings.remediate -eq $true) {
@@ -92,17 +111,23 @@ function Invoke-CIPPStandardEntraDeviceJoinScope {
                 }
             }
 
+            $Body = $CurrentInfo | Select-Object `
+                id,
+                userDeviceQuota,
+                multiFactorAuthConfiguration,
+                azureADRegistration,
+                azureADJoin,
+                localAdminPassword
+
+            $Body.azureADJoin.allowedToJoin = $AllowedToJoin
+
             $GraphParam = @{
                 tenantid    = $Tenant
-                Uri         = 'https://graph.microsoft.com/beta/policies/deviceRegistrationPolicy/deviceRegistrationPolicy'
+                Uri         = 'https://graph.microsoft.com/beta/policies/deviceRegistrationPolicy'
                 ContentType = 'application/json; charset=utf-8'
                 asApp       = $false
-                type        = 'PATCH'
-                Body        = @{
-                    azureADJoin = @{
-                        allowedToJoin = $AllowedToJoin
-                    }
-                } | ConvertTo-Json -Depth 10
+                type        = 'PUT'
+                Body        = $Body | ConvertTo-Json -Depth 20
             }
 
             try {
@@ -119,14 +144,21 @@ function Invoke-CIPPStandardEntraDeviceJoinScope {
         if ($StateIsCorrect -eq $true) {
             Write-LogMessage -API 'Standards' -Tenant $Tenant -Message 'Entra device join scope is correctly configured' -Sev Info
         } else {
-            Write-StandardsAlert -message 'Entra device join scope is not correctly configured' -object $CompareField -tenant $Tenant -standardName 'EntraDeviceJoinScope' -standardId $Settings.standardId
+            Write-StandardsAlert `
+                -message 'Entra device join scope is not correctly configured' `
+                -object $CompareField `
+                -tenant $Tenant `
+                -standardName 'EntraDeviceJoinScope' `
+                -standardId $Settings.standardId
+
             Write-LogMessage -API 'Standards' -Tenant $Tenant -Message 'Entra device join scope is not correctly configured' -Sev Info
         }
     }
 
     if ($Settings.report -eq $true) {
         $CurrentValue = @{
-            appliesTo = $CurrentAllowed.appliesTo
+            appliesTo   = $CurrentAllowed.appliesTo
+            customGroup = $CurrentGroupIds -join ','
         }
 
         $ExpectedValue = @{
@@ -134,7 +166,16 @@ function Invoke-CIPPStandardEntraDeviceJoinScope {
             customGroup = $Settings.customGroup ?? ''
         }
 
-        Set-CIPPStandardsCompareField -FieldName 'standards.EntraDeviceJoinScope' -CurrentValue $CurrentValue -ExpectedValue $ExpectedValue -TenantFilter $Tenant
-        Add-CIPPBPAField -FieldName 'EntraDeviceJoinScope' -FieldValue $StateIsCorrect -StoreAs bool -Tenant $Tenant
+        Set-CIPPStandardsCompareField `
+            -FieldName 'standards.EntraDeviceJoinScope' `
+            -CurrentValue $CurrentValue `
+            -ExpectedValue $ExpectedValue `
+            -TenantFilter $Tenant
+
+        Add-CIPPBPAField `
+            -FieldName 'EntraDeviceJoinScope' `
+            -FieldValue $StateIsCorrect `
+            -StoreAs bool `
+            -Tenant $Tenant
     }
 }
